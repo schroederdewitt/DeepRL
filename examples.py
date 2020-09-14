9bc5a5df8aa2d842cafd42eb782fb6e2a2e872b8
Christian Schroeer <schroederdewitt@gmail.com>
2020-09-15 00:49:05 +0100

    minor

--- examples.py
+++ examples.py
@@ -643,7 +643,7 @@ if __name__ == '__main__':
     # td3_continuous(game=game)
 
     game = 'BreakoutNoFrameskip-v4'
-    dqn_pixel(game=game, n_step=1, replay_cls=UniformReplay, async_replay=False)
+    dqn_pixel(game=game, save_interval=200000, n_step=1, replay_cls=UniformReplay, async_replay=False)
     # quantile_regression_dqn_pixel(game=game)
     # categorical_dqn_pixel(game=game)
     # rainbow_pixel(game=game, async_replay=False)
